160c87c4c3489cac09a703ee80917ff062061f6f
sheep96 <127265603+sheepjun96@users.noreply.github.com>
2024-05-11 12:25:28 +0900

    test

---
+++ flaskbook/apps/minimalapp/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return "Hello"
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello~ {name}"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
