27aa7ba3feb3ee6309d874f81229f40d30510436
sheep96 <127265603+sheepjun96@users.noreply.github.com>
2024-05-11 13:33:25 +0900

    last

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+
+from flask import Flask, render_template, url_for, current_app, g, request, redirect
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "yangjunmo5420"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,3 +16,17 @@ def hello(name):
 def show_name(name):
     return render_template("index.html", name=name)
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+        
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
