2cdbef493beed784c459b09284aedfee0ac3a974
sheep96 <127265603+sheepjun96@users.noreply.github.com>
2024-05-12 15:58:09 +0900

    last

--- flaskbook_2/apps/crud/views.py
+++ flaskbook_2/apps/crud/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from apps.crud.forms import UserForm
 from apps.app import db
 from apps.crud.models import User
 
@@ -17,3 +18,24 @@ def index():
 def sql():
     db.session.query(User).all()
     return "콘솔 로그를 확인해 주세요."
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
+
+@crud.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
