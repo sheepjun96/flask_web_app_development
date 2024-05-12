56fbc5c2f8b221d0136c8570b8dd6e8a54cbcc80
sheep96 <127265603+sheepjun96@users.noreply.github.com>
2024-05-12 15:31:05 +0900

    last

--- flaskbook_2/apps/app.py
+++ flaskbook_2/apps/app.py
@@ -1,7 +1,27 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
+        SQLALCHEMY_DATABASE_URI=
+            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            SQLALCHEMY_ECHO=True
+    )
+
+    csrf.init_app(app)
+    db.init_app(app)
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
 
